a585beaa9dd66e7dc7b1e8baa34924911b1c0949
Last-SilverLight-End <halfwing1998@gmail.com>
2022-08-21 18:01:17 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const debug = process.env.NODE_ENV !== 'production';
+const name = 'ShowMeTheHomepage';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: debug ? '' : `/${name}`
 }
 
 module.exports = nextConfig
