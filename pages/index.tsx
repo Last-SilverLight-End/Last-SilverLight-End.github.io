aacd4391301d412051ef2622fed3a371760261c3
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-08-22 09:30:02 +0900

    fix router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
 import type { NextPage } from 'next'
-import Link from 'next/link';
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { FC, useState } from 'react'
-
-
-
 
 const IntroduceButton:FC = () => {
   const router = useRouter();
 
-  const handleClick = () => { 
-    router.push("/test")
-  }
   return( 
-  
-    <a href='/test' onClick={handleClick}>
-
-      눌러 내 자기소개야
-      </a>
-
+  <button onClick={() => router.push("/test")}>
+    눌러 내 소개야
+  </button>
   )
 }
 
@@ -26,12 +16,9 @@ const GitLinkButton:FC = () => {
 
   const router = useRouter();
 
-  return(
-    
-  <a href="https://github.com/Last-SilverLight-End">
+  return <a href="https://github.com/Last-SilverLight-End">
     눌러 내 깃허브 링크야
-    </a>
-  )
+    </a>;
 }
 
 const Home: NextPage = () => {
@@ -43,7 +30,6 @@ const Home: NextPage = () => {
       <h2>자기소개</h2>
 
       <IntroduceButton />
-      <h1></h1>
       <GitLinkButton/>
     </div>
   )
