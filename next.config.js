70e4ce67b730eadd99e0de159456891036a51adb
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-08-24 21:05:55 +0900

    fix next.config.js in localhost

--- next.config.js
+++ next.config.js
@@ -9,13 +9,19 @@ const nextConfig = {
 }
 
 module.exports = {
-  basePath: `/${name}`,
+  basePath: 
+    process.env.NODE_ENV === "production"
+      ? "/ShowMeTheHomepage"
+      : undefined ,
+
   assetPrefix:
-  process.env.NODE_ENV === "production"
-    ? "https://last-silverlight-end.github.io/ShowMeTheHomepage/"
-    : "/ShowMeTheHomepage/",
-    reactStrictMode: true,
-    swcMinify: true,
+    process.env.NODE_ENV === "production"
+      ? "https://last-silverlight-end.github.io/ShowMeTheHomepage/"
+      : undefined,
+
+  reactStrictMode: true,
+
+  swcMinify: true,
   
 
 };
