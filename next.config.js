5238599d022da3a74e0db4b18518dfcb29eb14f8
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-10-22 23:02:26 +0900

    fix3 homepage issue

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   assetPrefix:
     process.env.NODE_ENV === "production"
-      ? "https://changgeun.strongtrash.com/ShowMeTheHomepage/"
+      ? "https://changgeun.strongtrash.com"
       : undefined,
 
   reactStrictMode: false,
