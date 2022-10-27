b09b8e1c1869df340138bb82aec78df645f2f979
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-10-28 02:19:37 +0900

    modify footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,25 +1,28 @@
-
-import Link from 'next/link';
-import { FC } from 'react';
 import style from '@styles/footer.module.css';
-const Footer: FC = () => {
-  const whatisToday = () =>{  
-    const year = new Date().getFullYear();
-    let month = new Date().getMonth();
-    let date = new Date().getDay();
-    let years = ""+year;
-    let months : string = "";
-    let dates: string = "";
-    if(date<10){
-      dates = "0"+date;
-    }
 
-    if(month<10){
-      months = "0"+month;
-    }
+const fillZero = (n: number, maxLength: number = 2) =>
+  n.toString().padStart(maxLength, '0')
 
-    const total = years+"." + months+"."+dates;
-    return total;
+const Footer: React.FC = () => {
+  const getToday = () => {
+    // ? 왜 각각 new Date()쓰심
+    // 각각쓰면 그
+    // 예를들어서 오후 11시 59분 59 .99초에 딱 이 코드가 실행되기 시작했는데
+    // 살짝 오래걸린다면 날짜가 언매치되는 불상사가 
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = fillZero(now.getMonth() + 1);
+    // 그리고 자스가 자바의 **한api를 무지성으로 따라해서
+    // getMonth의 **한점도 그대로 녹아들어있는데요
+    // 1월이면 0을 뱉습니다
+    const date = fillZero(now.getDate());
+
+    // ㄷㄷ
+    // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+
+
+    // 자 그리고 이것마저도 깔끔하게 만들수가 있는데요
+    return [ year, month, date ].join('.');
   }
 
   return (
@@ -37,7 +40,7 @@ const Footer: FC = () => {
           <li><a href="#">고객센터</a></li>
         </ul>
         <div className={style.address} >
-          Copyright © { whatisToday() } LEE CHANG_GEUN All rights reserved.
+          Copyright © { getToday() } LEE CHANG_GEUN All rights reserved.
         </div>
       </div>
     </footer>
