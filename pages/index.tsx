18679f458db42f88bc996e190856eca22a740672
Last-SilverLight-End <halfwing1998@gmail.com>
2023-05-04 14:52:21 +0900

    chore : 메인 content 구성 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,34 +61,59 @@ const Home: NextPage = () => {
  
       </div>
 
-      <div className={styles.cont_main}>
+      <div>
         <div className ={styles.cont_center}>
           <div>
-            <h1>Front-End Devleoper</h1>
-          A
-          B
-          C
+            <Image alt="myHandsome pictures" src="/images/HunterKillerSleep.png" width={120} height={120} />
+            <h1>Front-End Developer</h1>
+            <h1>Also Can Do Back-End & Android Develop</h1>
+
           </div>
           <hr className = {styles.lines}/>
           <div>
             <h1>My Awards</h1>
-            <h1>My Awards</h1>
-            <h1>My Awards</h1>
-          A
-          B
-          C
-          D
+            <div className = {styles.award_cont}>
+              <ul>
+                <li>
+                 MapleStoryWorld MSW HACKERTON 우수 수상
+                </li>
+                <li>
+                 a KSC 학부 논문 
+              "메타버스 기반 네일아트 제작 플랫폼 개발"
+               장려 입상
+                </li>
+              </ul>
+
+            </div>
+
+            <h2> Other works</h2>
+            <h4> SDS 삼성 알고리즘 특강 수료 &nbsp; 
+              / &nbsp; UCPC 코딩 대회 참여 ( 2020, 2021, 2022 ) </h4>
+            
           </div>
           <hr className = {styles.lines}/>
           <div>
             <h1>My License</h1>
-          A
+            <div className = {styles.award_cont} >
+              <ul>
+                <li>
+                OPIC IH
+                </li>
+                <li>
+                TOEIC 870
+                </li>
+                <li>
+                TOSC 1급
+                </li>
+              </ul>
+            </div>
+            <h4>계속 해서 CS 관련 자격증에 도전하고 있습니다!</h4> 
           </div>
           <hr className = {styles.lines}/>
           <div>
             <h1> Be Polite, Be Creative , Be Ambitious</h1>
-            <h3> I WILL DO MY BEST. THANK YOU!</h3>
-            <h3> For more Infomation, Contact me!</h3>
+            <h2> I WILL DO MY BEST. THANK YOU!</h2>
+            <h2> For more Infomation, Contact me!</h2>
 
           </div>
 
